da4f3431a9ec6ccbaa9de5ba79bcaa75b4a06903
Zhuzhiqiang <1316995875@qq.com>
2017-08-15 13:43:28 +0800

    测试内容3

--- app/src/main/java/com/wisdom/mavenjcenter/MainActivity.java
+++ app/src/main/java/com/wisdom/mavenjcenter/MainActivity.java
@@ -1,17 +1,133 @@
 package com.wisdom.mavenjcenter;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
-import com.wisdom.irefreshlayout.VRefreshLayout;
+import com.wisdom.irecyclerview.IRecyclerView;
+import com.wisdom.irecyclerview.ProgressStyle;
+
+import java.util.ArrayList;
+
+/**
+ * ================================================
+ * 作    者：朱志强 Github地址：https://github.com/ZZhiQiang/MavenJcenter
+ * 版    本：1.0
+ * 创建 日期：17/08/15
+ * 描    述：
+ * 修订 历史：
+ * ================================================
+ */
 
 public class MainActivity extends AppCompatActivity {
-  private VRefreshLayout vRefreshLayout;
+    private IRecyclerView mRecyclerView;
+    private MyAdapter mAdapter;
+    private ArrayList<String> listData;
+    private int refreshTime = 0;
+    private int times = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // this  is  test
-        //多种测试内容
+        setContentView(R.layout.alpha_change_layout);
+        mRecyclerView = (IRecyclerView)this.findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setRefreshProgressStyle(ProgressStyle.BallSpinFadeLoader);
+        mRecyclerView.setLoadingMoreProgressStyle(ProgressStyle.BallRotate);
+        mRecyclerView.setArrowImageView(R.drawable.iconfont_downgrey);
+
+        /** ----- alpha change listen test start ----- */
+        final RelativeLayout alpha_title
+                = (RelativeLayout) findViewById(R.id.alpha_title);
+        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        findViewById(R.id.back_to_top).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRecyclerView.scrollToPosition(0);
+            }
+        });
+        mRecyclerView.setScrollAlphaChangeListener(new IRecyclerView.ScrollAlphaChangeListener() {
+            @Override
+            public void onAlphaChange(int alpha) {
+                alpha_title.getBackground().setAlpha(alpha);
+            }
+
+            @Override
+            public int setLimitHeight() {
+                return 1300;
+            }
+        });
+        /** ------ alpha change listen test end ----- */
+
+        mRecyclerView.setLoadingListener(new IRecyclerView.LoadingListener() {
+            @Override
+            public void onRefresh() {
+                refreshTime ++;
+                times = 0;
+                new Handler().postDelayed(new Runnable(){
+                    public void run() {
+                        listData.clear();
+                        for(int i = 0; i < 15 ;i++){
+                            listData.add("item" + i + "after " + refreshTime + " times of refresh");
+                        }
+                        mAdapter.notifyDataSetChanged();
+                        mRecyclerView.refreshComplete();
+                    }
+
+                }, 1000);            //refresh data here
+            }
+
+            @Override
+            public void onLoadMore() {
+                if(times < 2){
+                    new Handler().postDelayed(new Runnable(){
+                        public void run() {
+                            for(int i = 0; i < 15 ;i++){
+                                listData.add("item" + (1 + listData.size() ) );
+                            }
+                            mRecyclerView.loadMoreComplete();
+                            mAdapter.notifyDataSetChanged();
+                        }
+                    }, 1000);
+                } else {
+                    new Handler().postDelayed(new Runnable() {
+                        public void run() {
+                            for(int i = 0; i < 9 ;i++){
+                                listData.add("item" + (1 + listData.size() ) );
+                            }
+                            mRecyclerView.setNoMore(true);
+                            mAdapter.notifyDataSetChanged();
+                        }
+                    }, 1000);
+                }
+                times ++;
+            }
+        });
+
+        listData = new  ArrayList<String>();
+        mAdapter = new MyAdapter(listData);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.refresh();
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
